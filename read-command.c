59857ffd6641ea6b4872b7e4c198609c8de079e4
Saurabh S Davala <saurabh@lnxsrv05.seas.ucla.edu>
2014-01-13 16:31:29 -0800

    initial structs

--- read-command.c
+++ read-command.c
@@ -1,9 +1,10 @@
 // UCLA CS 111 Lab 1 command reading
-
 #include "command.h"
 #include "command-internals.h"
 
 #include <error.h>
+
+#include <stdio.h>
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
@@ -11,13 +12,20 @@
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
 
+enum token_id  {
+  semicolon, pipe, and, or, start_subshell, close_subshell, in, out, word, nil
+};
+
+struct token_list_element {
+  enum token_id id;
+  char* body;
+};
+
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
-		     void *get_next_byte_argument)
+         void *get_next_byte_argument)
 {
-  /* FIXME: Replace this with your implementation.  You may need to
-     add auxiliary functions and otherwise modify the source code.
-     You can also use external functions defined in the GNU C Library.  */
+  printf("%c\n", get_next_byte(get_next_byte_argument));
   error (1, 0, "command reading not yet implemented");
   return 0;
 }
